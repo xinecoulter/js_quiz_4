7cb2a1576f8f4e656474dc99999328e189939a82
Phillip Lamplugh <phillip.lamplugh@gmail.com>
2013-08-29 00:54:58 -0400

    first

---
+++ readme.md
@@ -0,0 +1,15 @@
+# This is a quiz on JavaScript and writing tests!
+
+Please see `src/quiz.js` and `spec/quizSpec.js`.
+
+The first tests for the first question are already written.
+COOL!
+
+Write the corresponding code.
+
+To run your tests open `SpecRunner.html`.
+
+For the remaining questions write __both__ the tests and functions.
+AGAIN?!
+
+__Please replace this line with your name.__
---
+++ spec/quizSpec.js
@@ -0,0 +1,24 @@
+//      ___       _______      ___       __  .__   __.  ______    __
+//     /   \     /  _____|    /   \     |  | |  \ |  | |      \  |  |
+//    /  ^  \   |  |  __     /  ^  \    |  | |   \|  | `----)  | |  |
+//   /  /_\  \  |  | |_ |   /  /_\  \   |  | |  . `  |     /  /  |  |
+//  /  _____  \ |  |__| |  /  _____  \  |  | |  |\   |    |__|   |__|
+// /__/     \__\ \______| /__/     \__\ |__| |__| \__|     __    (__)
+//                                                        (__)
+
+// See quiz.js for more details
+
+describe('a quiz', function() {
+
+  it('is about JavaScript and testing with Jasmine', function() {
+    expect(QUIZ_TOPICS).toContain('jasmine');
+    expect(QUIZ_TOPICS).toContain('javascript');
+  });
+
+  it('has a sorting method', function() {
+    var array = [5, 7, 10, 2, 4, 3, 6, 8, 1, 9];
+    var sorted = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    expect(array.sort()).toEqual(sorted);
+  })
+});
+
