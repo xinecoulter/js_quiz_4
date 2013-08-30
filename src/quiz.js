6b6bf68d1248f72d3b1ea7258d687fe7080b3c16
Christine Coulter <christinetcoulter@gmail.com>
2013-08-30 10:33:28 -0400

    It sortsgit add .git add .git add .git add .

--- src/quiz.js
+++ src/quiz.js
@@ -6,3 +6,32 @@
 //      |__|     |__|  |__| |__| |_______/       |__| |_______/       /__/     \__\     \_____\_____\\______/  |__|  /________(__)
 
 var QUIZ_TOPICS = ['javascript', 'jasmine', 'testing'];
+
+Array.prototype.sort = function() {
+
+    function compare(a, b) {
+        if (a < b) {
+            return -1;
+        } else if (a > b) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    for (var i = 0; i < this.length; i++) {
+        var compareValue = compare(this[i], this[i+1]);
+        if (compareValue === -1) {
+            console.log(this[i] + " is less than " + this[i+1]);
+        } else if (compareValue === 1) {
+            var switch1 = this[i];
+            var switch2 = this[i+1];
+            this[i] = switch2;
+            this[i+1] = switch1;
+            this.sort();
+            break;
+        }
+    }
+
+    return this;
+};
